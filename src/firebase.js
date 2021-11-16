3c49d4c487e82d59e70d7c23561f1f65425e9dd7
Chris Wren <dazedpandas@gmail.com>
2021-11-16 13:39:42 +0000

    Initial Project Files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC-0EKfGv0lFdU3vEir2OCp4L4ldlJtMc4",
+  authDomain: "prism-86f7a.firebaseapp.com",
+  projectId: "prism-86f7a",
+  storageBucket: "prism-86f7a.appspot.com",
+  messagingSenderId: "604982199144",
+  appId: "1:604982199144:web:29bb1793a367383735ccdd",
+};
